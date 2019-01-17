ce69b1d4acae2f2693f5d8482b0652cb55700642
Dillon Mabry <dcmabry@gmail.com>
2019-01-16 22:25:14 -0500

    Update webpack config for travis

--- webpack.config.js
+++ webpack.config.js
@@ -5,72 +5,109 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const dotenv = require('dotenv');
 const outputDirectory = 'dist';
 
-const env = dotenv.config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+module.exports = (env, options) => {
+  // DEV
+  if (options.mode === 'development') {
+    const env = dotenv.config().parsed;
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(env[next]);
+      return prev;
+    }, {});
 
-const webPackConfig = {
-  entry: ['babel-polyfill', './src/client/index.js'],
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+    return {
+      entry: ['babel-polyfill', './src/client/index.js'],
+      output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'bundle.js',
+        publicPath: '/'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+          }
+        ]
+      },
+      devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+          '/api': 'http://localhost:5000'
+        },
+        historyApiFallback: true
+      },
+      plugins: [
+        new CleanWebpackPlugin([outputDirectory]),
+        new HtmlWebpackPlugin({
+          template: './public/index.html'
+        }),
+        new webpack.DefinePlugin(envKeys)
+      ],
+      resolve: {
+        alias: {
+          'react-virtualized/List': 'react-virtualized/dist/es/List',
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
       }
-    ]
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:5000'
-    },
-    historyApiFallback: true
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
-    new webpack.DefinePlugin(envKeys)
-  ],
-  resolve: {
-    alias: {
-      'react-virtualized/List': 'react-virtualized/dist/es/List',
+    }
+  // PROD
+  } else {
+    return {
+      entry: ['babel-polyfill', './src/client/index.js'],
+      output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'bundle.js',
+        publicPath: '/'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+          }
+        ]
+      },
+      devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+          '/api': 'http://localhost:5000'
+        },
+        historyApiFallback: true
+      },
+      plugins: [
+        new CleanWebpackPlugin([outputDirectory]),
+        new HtmlWebpackPlugin({
+          template: './public/index.html'
+        })
+      ],
+      resolve: {
+        alias: {
+          'react-virtualized/List': 'react-virtualized/dist/es/List',
+        }
+      }
     }
   }
 };
-
-// Dev
-if(envKeys) {
-  module.exports = webPackConfig;
-// Prod (remove env keys)
-} else {
-  let prodWebPackConfig = webPackConfig.plugins = [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
-  module.exports = prodWebPackConfig;
-}
-
-
